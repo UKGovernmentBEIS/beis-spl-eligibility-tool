13b1730d9e2688549b514d2ac5fa364435d3a0cf
MatthewBurstein <matthew.burstein@softwire.com>
2019-05-15 12:07:28 +0100

    add relevantWeek filter with tests

--- app/filters.js
+++ app/filters.js
@@ -1,26 +1,40 @@
 // Existing filters can be imported from env using env.getFilter(name)
 // See https://mozilla.github.io/nunjucks/api.html#getfilter
-const { convertToMoment, formatForDisplay, standardFormat } = require('../common/utils/dateUtils')
+
+const moment = require('moment')
+const dates = require('../common/utils/dateUtils')
 
 module.exports = function (env) {
-  function twentySixWeeksBeforeRelevantWeek (data) {
+  function relevantWeek (data) {
     const {
       'start-date-day': day,
       'start-date-month': month,
       'start-date-year': year
     } = data
 
-    const startOfWeekProvided = convertToMoment(year, month, day).startOf('week')
+    const startOfWeekProvided = dates.convertToMoment(year, month, day).startOf('week')
     const qualifyingWeek = env.getFilter('isBirth')(data) ? startOfWeekProvided.subtract(105, 'days') : startOfWeekProvided
-    return standardFormat(qualifyingWeek)
+    return dates.standardFormat(qualifyingWeek)
   }
 
-  function displayFormat (standardFormatDate) {
-    return formatForDisplay(standardFormatDate)
+  function twentySixWeeksBeforeRelevantWeek (data) {
+    const relevantWeekValue = relevantWeek(data)
+    const twentySixWeeksBefore = moment(relevantWeekValue).subtract(26 * 7, 'days')
+    return dates.standardFormat(twentySixWeeksBefore)
+  }
+
+  function formatForDisplay (standardFormatDate) {
+    return dates.formatForDisplay(standardFormatDate)
+  }
+
+  function isInPast (standardFormatDate) {
+    return dates.isInPast(standardFormatDate)
   }
 
   return {
+    relevantWeek,
     twentySixWeeksBeforeRelevantWeek,
-    displayFormat
+    formatForDisplay,
+    isInPast
   }
 }
--- app/filters.test.js
+++ app/filters.test.js
@@ -14,7 +14,7 @@ describe('filters', () => {
     filters = require('./filters')(environment)
   })
 
-  describe('twentySixWeeksBeforeRelevantWeek', () => {
+  describe('relevantWeek', () => {
     it('returns the start of the match week for adopters', () => {
       sinon.stub(environment, 'getFilter').withArgs('isBirth').returns(() => false)
 
@@ -24,10 +24,10 @@ describe('filters', () => {
         'start-date-year': '2019'
       }
 
-      expect(filters.twentySixWeeksBeforeRelevantWeek(data)).to.equal('2019-09-29')
+      expect(filters.relevantWeek(data)).to.equal('2019-09-29')
     })
 
-    it('returns 105 days before the start of the birth week for birth parents', () => {
+    it('returns 15 weeks before the start of the birth week for birth parents', () => {
       sinon.stub(environment, 'getFilter').withArgs('isBirth').returns(() => true)
 
       const data = {
@@ -36,7 +36,7 @@ describe('filters', () => {
         'start-date-year': '2019'
       }
 
-      expect(filters.twentySixWeeksBeforeRelevantWeek(data)).to.equal('2019-06-16')
+      expect(filters.relevantWeek(data)).to.equal('2019-06-16')
     })
 
     it('always returns a sunday', () => {
@@ -48,13 +48,39 @@ describe('filters', () => {
         'start-date-year': '2019'
       }
 
-      expect(filters.twentySixWeeksBeforeRelevantWeek(dataWithSunday)).to.equal('2019-09-29')
+      expect(filters.relevantWeek(dataWithSunday)).to.equal('2019-09-29')
     })
   })
 
-  describe('displayFormat', () => {
+  describe('formatForDisplay', () => {
     it("returns a string in the form '1 December 2019'", () => {
-      expect(filters.displayFormat('2019-10-09')).to.equal('9 October 2019')
+      expect(filters.formatForDisplay('2019-10-09')).to.equal('9 October 2019')
+    })
+  })
+
+  describe('twentySixWeeksBeforeRelevantWeek', () => {
+    it('returns 26 weeks before the start of the match week for adopters', () => {
+      sinon.stub(environment, 'getFilter').withArgs('isBirth').returns(() => false)
+
+      const data = {
+        'start-date-day': '01',
+        'start-date-month': '10',
+        'start-date-year': '2019'
+      }
+
+      expect(filters.twentySixWeeksBeforeRelevantWeek(data)).to.equal('2019-03-31')
+    })
+
+    it('returns 26 weeks before 15 weeks before the start of the birth week for birth parents', () => {
+      sinon.stub(environment, 'getFilter').withArgs('isBirth').returns(() => true)
+
+      const data = {
+        'start-date-day': '01',
+        'start-date-month': '10',
+        'start-date-year': '2019'
+      }
+
+      expect(filters.twentySixWeeksBeforeRelevantWeek(data)).to.equal('2018-12-16')
     })
   })
 })
