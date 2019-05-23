fcb6e95b9707efb53c5defb9ac61a7fbc707f587
MatthewBurstein <matthew.burstein@softwire.com>
2019-05-23 11:37:31 +0100

    implement step-validation middleware

--- app/step-validation.js
+++ app/step-validation.js
@@ -2,31 +2,24 @@ const paths = require('./paths')
 const validate = require('./validate')
 
 module.exports = function (req, res, next) {
-  const stepIndex = paths.getIndex(req.url)
-  if (stepIndex === 0) { return next() }
-  const indexOfEarliestErrorPage = validateHistory(req, stepIndex - 1)
-  console.log(indexOfEarliestErrorPage)
-  return indexOfEarliestErrorPage > -1 ? res.redirect(paths.getPath(indexOfEarliestErrorPage)) : next()
-}
-
-function validateHistory (req, index) {
-  let errorAtIndex
-  console.log('index')
-  console.log(index)
-  if (index > 0) {
-    errorAtIndex = validateHistory(req, index - 1)
-    console.log('in if')
-    console.log(errorAtIndex)
-    if (errorAtIndex > -1) { return errorAtIndex }
+  const earliestPathWithValidationErrors = getEarliestPathWithValidationErrors(paths.getPreviousPath(req.path), req)
+  if (earliestPathWithValidationErrors) {
+    res.redirect(earliestPathWithValidationErrors)
+  } else {
+    next()
   }
-  console.log('route')
-  console.log(paths.getName(index))
-
-  const stepName = paths.getName(index)
-  const isStepValid = stepHasValidator(stepName) ? validate[stepName](req) : true
-  return isStepValid ? -1 : index
 }
 
-function stepHasValidator (stepName) {
-  return !!validate[stepName]
+function getEarliestPathWithValidationErrors (path, req) {
+  if (!path) {
+    return null
+  }
+  const previousPath = paths.getPreviousPath(path)
+  const earliestPathWithValidationErrors = getEarliestPathWithValidationErrors(previousPath, req)
+  if (earliestPathWithValidationErrors) {
+    return earliestPathWithValidationErrors
+  }
+  const pathName = paths.getName(path)
+  const isValid = validate[pathName] ? validate[pathName](req) : true
+  return isValid ? null : paths.getPath(pathName)
 }
