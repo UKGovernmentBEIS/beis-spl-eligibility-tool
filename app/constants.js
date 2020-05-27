252faa68c2e20e8bde778e6d2080d8461bb5140d
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-05-27 13:10:32 +0100

    SPL-211: Remove capitalisation from Shared Parental Leave and Pay

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,4 @@
 module.exports = {
-  SERVICE_NAME: 'Check if you can get Shared Parental Leave and Pay',
+  SERVICE_NAME: 'Check if you can get shared parental leave and pay',
   MINIMUM_PAY_THRESHOLD: 390
 }
