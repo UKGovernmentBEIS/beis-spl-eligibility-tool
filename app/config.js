10d750da7db8e2c123ebd19db8fda8fde671f2d5
Aaron Lambley <aaron.lambley@gmail.com>
2024-10-03 09:52:26 +0100

    setup ASIM formatted logger

---
+++ app/config.js
@@ -0,0 +1,8 @@
+const { NODE_ENV, LOG_LEVEL } = process.env
+const config = {
+  isProd: NODE_ENV === 'production',
+  isDev: NODE_ENV === 'development',
+  isTest: NODE_ENV === 'test',
+  logLevel: LOG_LEVEL || 'info'
+}
+module.exports = config
