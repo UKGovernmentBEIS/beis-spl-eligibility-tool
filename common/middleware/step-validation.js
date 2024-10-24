8e726615e8ba323a3470c1c1c2e8724f0947f756
Aaron Lambley <aaron.lambley@gmail.com>
2024-10-24 10:07:16 +0100

    add common middleware tests

--- common/middleware/step-validation.js
+++ common/middleware/step-validation.js
@@ -1,10 +1,17 @@
 const paths = require('../../app/paths')
 
 module.exports = function (req, res, next) {
-  const previousWorkflowPath = paths.getPreviousWorkflowPath(req.path, req.session.data)
-  const earliestPathWithValidationErrors = getEarliestPathWithValidationErrors(previousWorkflowPath, req)
+  const previousWorkflowPath = paths.getPreviousWorkflowPath(
+    req.path,
+    req.session.data
+  )
+  const earliestPathWithValidationErrors = getEarliestPathWithValidationErrors(
+    previousWorkflowPath,
+    req
+  )
 
-  const redirectDisabled = (process.env.NODE_ENV !== 'production' && process.env.PA11Y_TEST === 'true')
+  const redirectDisabled =
+    process.env.NODE_ENV !== 'production' && process.env.PA11Y_TEST === 'true'
 
   if (earliestPathWithValidationErrors && !redirectDisabled) {
     res.redirect(earliestPathWithValidationErrors)
@@ -13,18 +20,39 @@ module.exports = function (req, res, next) {
   }
 }
 
-function getEarliestPathWithValidationErrors (path, req) {
+function getEarliestPathWithValidationErrors (
+  path,
+  req,
+  visitedPaths = new Set()
+) {
   if (!path) {
     return null
   }
 
-  const previousPath = paths.getPreviousWorkflowPath(path, req.session.data)
-  const earliestPathWithValidationErrors = getEarliestPathWithValidationErrors(previousPath, req)
+  if (visitedPaths.has(path)) {
+    console.warn(`Circular reference detected for path: ${path}`)
+    return null
+  }
+
+  visitedPaths.add(path)
+
+  const previousPath = paths.getPreviousWorkflowPath(
+    path,
+    req.session.data,
+    true
+  )
+  const earliestPathWithValidationErrors = getEarliestPathWithValidationErrors(
+    previousPath,
+    req,
+    visitedPaths
+  )
+
   if (earliestPathWithValidationErrors) {
     return earliestPathWithValidationErrors
   }
 
   const validator = paths.getValidator(path)
   const isValid = validator ? validator(req) : true
+
   return isValid ? null : path
 }
