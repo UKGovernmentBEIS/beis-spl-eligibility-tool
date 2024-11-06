3a2ddb9df97b15d684b496427e801e06fd619fc2
Devin Rathod <119806596+devind-ra@users.noreply.github.com>
2024-11-06 09:11:26 +0000

    SPL-464 - Update cookie banner (#145)
    * moving files into test folder following PT structure
    
    * updating cookie banner
    
    * fixing pageTitle consistency

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,4 @@
 module.exports = {
-  SERVICE_NAME: 'Check if you can get shared parental leave and pay',
+  SERVICE_NAME: 'Check if you can get Shared Parental Leave and Pay',
   MINIMUM_PAY_THRESHOLD: 390
 }
